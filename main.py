beb36772701628587a492d5e9d68bf9ce8f1780a
Birkity <85987355+Birkity@users.noreply.github.com>
2025-01-07 17:06:30 +0300

    Generate a random individual (binary string of length n).

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ def knapsack_fitness(individual, weights, values, weight_limit):
     total_weight = sum(individual[i] * weights[i] for i in range(len(individual)))
     total_value = sum(individual[i] * values[i] for i in range(len(individual)))
     return total_value if total_weight <= weight_limit else 0
+
+# ----------------------------
+# Step 2: Generate Random Individual
+# ----------------------------
+def random_individual(n):
+    return [random.randint(0, 1) for _ in range(n)]
